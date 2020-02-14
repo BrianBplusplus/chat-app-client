76867271405c0000979b13016abc971e77f5470a
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-14 13:49:09 +0100

    stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  stream = new EventSource("http://localhost:4000/stream");
+
+  componentDidMount() {
+    this.stream.onmessage = function(event) {
+      console.log("event test ", event);
+
+      const { data } = event;
+      console.log("data test", data);
+
+      const parsed = JSON.parse(data);
+      console.log("parsed test", parsed);
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Yo!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
